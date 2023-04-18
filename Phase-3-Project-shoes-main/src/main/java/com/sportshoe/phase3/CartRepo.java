fa5fb4fe45b85bb875ec9f6211930279c3459975
Shreyas N <shreyaskumar333@gmail.com>
2023-04-18 11:40:48 +0530

    changes

---
+++ Phase-3-Project-shoes-main/src/main/java/com/sportshoe/phase3/CartRepo.java
@@ -0,0 +1,7 @@
+package com.sportshoe.phase3;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepo extends JpaRepository<Cart, Integer>{
+
+}
---
+++ Phase-3-Project-shoes-main/src/test/java/com/sportshoe/phase3/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportshoe.phase3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
